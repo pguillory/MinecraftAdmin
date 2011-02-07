14b37a6fbe777fe36a209bff9f5792d448e7a587
Preston Guillory <pguillory@gmail.com>
2011-02-07 02:01:02 -0600

    Renamed config setting MinecraftOverviewer.every_seconds to MinecraftOverviewer.run_every_seconds

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ conf.Minecraft.start_memory = conf.Minecraft.start_memory || '1024M'
 conf.Minecraft.max_memory = conf.Minecraft.max_memory || '1024M'
 conf.MinecraftOverviewer.map_path  = conf.MinecraftOverviewer.map_path || path.join(conf.Minecraft.path, 'map')
 conf.MinecraftOverviewer.cache_path  = conf.MinecraftOverviewer.cache_path || path.join(conf.Minecraft.path, 'mapcache')
-conf.MinecraftOverviewer.every_seconds  = conf.MinecraftOverviewer.every_seconds || 60
+conf.MinecraftOverviewer.run_every_seconds  = conf.MinecraftOverviewer.run_every_seconds || 60
 
 new MinecraftAdmin.Server(conf)

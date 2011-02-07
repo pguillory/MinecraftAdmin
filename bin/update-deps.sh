a0f9d2c43cbd9c8eb8468062cedea51ea080d2de
Preston Guillory <pguillory@gmail.com>
2011-02-07 01:58:53 -0600

    Made update-deps.sh more robust.

--- bin/update-deps.sh
+++ bin/update-deps.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+set -e
 cd `dirname $0`
-cd ../deps
-rm -rf *
-git clone git@github.com:pguillory/node-iniconf.git iniconf
-git clone https://github.com/mozilla/narcissus.git
-git clone https://github.com/Sage/streamlinejs.git
-git clone git@github.com:pguillory/node-web.git web
+cd ..
+rm -rf deps-temp
+mkdir deps-temp
+pushd deps-temp
+git clone git@github.com:pguillory/node-iniconf.git  iniconf
+git clone https://github.com/mozilla/narcissus.git   narcissus
+git clone https://github.com/Sage/streamlinejs.git   streamlinejs
+git clone git@github.com:pguillory/node-web.git      web
 find . -name .git | xargs rm -rf
+popd
+rm -rf deps
+mv deps-temp deps
 git add .
 git ls-files -d -z | xargs -0 git update-index --remove

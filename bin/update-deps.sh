fb27c0de65760598b5a81b9a0c3db5a2df5bead3
Preston Guillory <pguillory@gmail.com>
2011-02-07 02:03:47 -0600

    update-deps.sh checks out read-only copies

--- bin/update-deps.sh
+++ bin/update-deps.sh
@@ -5,10 +5,10 @@ cd ..
 rm -rf deps-temp
 mkdir deps-temp
 pushd deps-temp
-git clone git@github.com:pguillory/node-iniconf.git  iniconf
-git clone https://github.com/mozilla/narcissus.git   narcissus
-git clone https://github.com/Sage/streamlinejs.git   streamlinejs
-git clone git@github.com:pguillory/node-web.git      web
+git clone https://github.com/pguillory/node-iniconf.git  iniconf
+git clone https://github.com/mozilla/narcissus.git       narcissus
+git clone https://github.com/Sage/streamlinejs.git       streamlinejs
+git clone https://github.com/pguillory/node-web.git      web
 find . -name .git | xargs rm -rf
 popd
 rm -rf deps

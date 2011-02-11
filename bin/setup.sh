c3c9cf0b81cc8ec439b8d45a1e1805ca29bd573d
Preston Guillory <pguillory@gmail.com>
2011-02-11 17:16:44 -0600

    Moved setup script into bin dir.

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,12 +3,12 @@ set -e
 apt-get update; apt-get upgrade -y
 apt-get install -y git subversion
 
-echo "*** Installing Node.js v0.2.6"
+echo "*** Installing Node.js v0.3.8"
 apt-get -y install build-essential libssl-dev
 cd /usr/local/src
-wget http://nodejs.org/dist/node-v0.2.6.tar.gz
-tar zxvf node-v0.2.6.tar.gz
-cd node-v0.2.6
+wget http://nodejs.org/dist/node-v0.3.8.tar.gz
+tar zxvf node-v0.3.8.tar.gz
+cd node-v0.3.8
 ./configure
 make; make install
 
@@ -16,13 +16,17 @@ echo "*** Installing Minecraft"
 apt-get install -y openjdk-6-jre-headless
 mkdir /mnt/minecraft
 cd /mnt/minecraft
-curl "http://www.minecraft.net/download/minecraft_server.jar?v=1296774514651" > minecraft_server.jar
+curl "http://www.minecraft.net/download/minecraft_server.jar" > minecraft_server.jar
 
 echo "*** Installing Minecraft-Overviewer"
 apt-get install -y python-numpy python-imaging
 cd /mnt
 git clone https://github.com/brownan/Minecraft-Overviewer.git
+mkdir map
+mkdir mapcache
 
 echo "*** Installing MinecraftAdmin"
 cd /mnt
 git clone https://github.com/pguillory/MinecraftAdmin.git
+cd MinecraftAdmin
+cp config/config.ini-production config/config.ini
